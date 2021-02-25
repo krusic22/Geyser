3b6ca19d7df3bfeb90f71ad4ea7f1c0260ae2ceb
BuildTools <unconfigured@null.spigotmc.org>
2021-02-25 20:23:39 +0100

    Change locale.

--- connector/src/main/java/org/geysermc/connector/utils/LoginEncryptionUtils.java
+++ connector/src/main/java/org/geysermc/connector/utils/LoginEncryptionUtils.java
@@ -178,7 +178,7 @@ public class LoginEncryptionUtils {
         String userLanguage = session.getLocale();
         CustomFormWindow window = new CustomFormBuilder(LanguageUtils.getPlayerLocaleString("geyser.auth.login.form.details.title", userLanguage))
                 .addComponent(new LabelComponent(LanguageUtils.getPlayerLocaleString("geyser.auth.login.form.details.desc", userLanguage)))
-                .addComponent(new InputComponent(LanguageUtils.getPlayerLocaleString("geyser.auth.login.form.details.email", userLanguage), "Uporabnisko ime", ""))
+                .addComponent(new InputComponent(LanguageUtils.getPlayerLocaleString("geyser.auth.login.form.details.email", userLanguage), "Username", ""))
                 .build();
 
         session.sendForm(window, AUTH_DETAILS_FORM_ID);
