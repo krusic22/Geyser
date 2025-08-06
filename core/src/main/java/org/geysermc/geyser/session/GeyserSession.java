7d0d76836554e20e968fc9f0676d48f6fa165f0b
Kristjan Krušič <kristjan.krusic@gmail.com>
2025-08-06 02:21:16 +0200

    Still officially unsupported.

--- core/src/main/java/org/geysermc/geyser/session/GeyserSession.java
+++ core/src/main/java/org/geysermc/geyser/session/GeyserSession.java
@@ -2355,7 +2355,7 @@ public class GeyserSession implements GeyserConnection, GeyserCommandSource {
 
     @Override
     public @NonNull String bedrockUsername() {
-        return authData.name();
+        return protocol.getProfile().getName();
     }
 
     @Override
